d15100cfc81d2a80abc2d47c27381465a08a8a73
Glenn Sabales <glenn.sabales@novare.com.hk>
2019-12-10 13:42:19 +0800

    Added  modifications

--- movie-info-service/src/main/java/io/javabrains/movieinfoservice/api/MovieResource.java
+++ movie-info-service/src/main/java/io/javabrains/movieinfoservice/api/MovieResource.java
@@ -2,17 +2,27 @@ package io.javabrains.movieinfoservice.api;
 
 
 import io.javabrains.movieinfoservice.model.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${api.key}")
+    private String apiKey;
+
     @GetMapping("/{movieId}")
     public Movie getMovieInfo(@PathVariable String movieId) {
-        return new Movie(movieId, "Titanic");
+        Movie movie = restTemplate.getForObject("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + apiKey, Movie.class);
+        return new Movie(movieId, movie.getTitle(), movie.getOverview(), movie.getRelease_date());
     }
 }
--- movie-info-service/src/main/java/io/javabrains/movieinfoservice/model/Movie.java
+++ movie-info-service/src/main/java/io/javabrains/movieinfoservice/model/Movie.java
@@ -1,28 +1,21 @@
 package io.javabrains.movieinfoservice.model;
 
+import lombok.Data;
+
+@Data
 public class Movie {
 
-    private String movieId;
-    private String name;
+    private String id;
+    private String title;
+    private String overview;
+    private String release_date;
 
-    public Movie(String movieId, String name) {
-        this.movieId = movieId;
-        this.name = name;
-    }
+    private Movie() {}
 
-    public String getMovieId() {
-        return movieId;
-    }
-
-    public void setMovieId(String movieId) {
-        this.movieId = movieId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    public Movie(String id, String title, String overview, String release_date) {
+        this.id = id;
+        this.title = title;
+        this.overview = overview;
+        this.release_date = release_date;
     }
 }
