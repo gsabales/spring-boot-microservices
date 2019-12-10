d329ecb44df7a8f280b5e335b5076161a8e143e3
Glenn Sabales <glenn.sabales@novare.com.hk>
2019-12-10 14:01:07 +0800

    Extracted methods

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/api/CatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/api/CatalogResource.java
@@ -3,9 +3,10 @@ package io.javabrains.moviecatalogservice.api;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import io.javabrains.moviecatalogservice.model.CatalogItem;
 import io.javabrains.moviecatalogservice.model.Movie;
+import io.javabrains.moviecatalogservice.model.Rating;
 import io.javabrains.moviecatalogservice.model.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,22 +24,26 @@ public class CatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
-    @Autowired
-    private Environment env;
+    @Value("${ratings-data-service.url}")
+    private String ratingsDataServicePath;
+
+    @Value("${movie-info-service.url}")
+    private String movieInfoServicePath;
 
     @GetMapping("/{userId}")
     @HystrixCommand(fallbackMethod = "getFallbackCatalog")
     public  List<CatalogItem> getCatalog(@PathVariable String userId) {
+        UserRating ratings = getUserRating(userId);
+        return ratings.getUserRating().stream().map(rating -> getCatalogItem(movieInfoServicePath, rating)).collect(Collectors.toList());
+    }
 
-        String ratingsDataServicePath = env.getProperty("ratings-data-service.url");
-        String movieInfoServicePath = env.getProperty("movie-info-service.url");
-        UserRating ratings = restTemplate.getForObject(ratingsDataServicePath + userId, UserRating.class);
+    private UserRating getUserRating(@PathVariable String userId) {
+        return restTemplate.getForObject(ratingsDataServicePath + userId, UserRating.class);
+    }
 
-        return ratings.getUserRating().stream().map(rating -> {
-            Movie movie = restTemplate.getForObject(movieInfoServicePath + rating.getMovieId(), Movie.class);
-            return new CatalogItem(movie.getTitle(), "Sample Description", rating.getRating());
-        }).collect(Collectors.toList());
-
+    private CatalogItem getCatalogItem(String movieInfoServicePath, Rating rating) {
+        Movie movie = restTemplate.getForObject(movieInfoServicePath + rating.getMovieId(), Movie.class);
+        return new CatalogItem(movie.getTitle(), movie.getOverview(), rating.getRating());
     }
 
     public List<CatalogItem> getFallbackCatalog(@PathVariable String userId) {
